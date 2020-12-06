648185a4b1004c5c26e18ce97bb909b4dee68a60
Belousow Makc <lib.bmw@gmail.com>
2020-12-06 16:17:42 +0300

    Update install

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 BASEDIR=$(dirname "$0")
-echo "$BASEDIR"
-basedir=$(pwd -L)
-echo $basedir
 
-#git clone https://github.com/kristijanhusak/vim-packager ~/.config/nvim/pack/packager/opt/vim-packager
-#cargo install --force bat
-#cargo install --force skim
-#nvim +PackagerInstall +qa
-#nvim +'TSInstall rust python' +qa
-#nvim +TSUpdate +qa
+echo "Install NEOVIM configuration"
+
+cp -rf $BASEDIR/neovim ~/.config/nvim
+git clone https://github.com/kristijanhusak/vim-packager ~/.config/nvim/pack/packager/opt/vim-packager
+cargo install --force bat
+cargo install --force skim
+nvim +PackagerInstall +qa
+nvim +'TSInstall rust python' +qa
+nvim +TSUpdate +qa
