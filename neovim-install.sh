a0672ea3b055ae1bd3e5ec570a83c2af2e85ebfb
Belousov Maksim <lib.bmw@gmail.com>
2022-07-22 00:06:05 +0300

    Update neovim-install.sh

--- neovim-install.sh
+++ neovim-install.sh
@@ -4,10 +4,10 @@ BASEDIR=$(dirname "$0")
 
 echo "Install NEOVIM configuration"
 
-cp -rf $BASEDIR/neovim ~/.config/nvim
+ln -s $BASEDIR/neovim ~/.config/nvim
 git clone https://github.com/kristijanhusak/vim-packager ~/.config/nvim/pack/packager/opt/vim-packager
-cargo install --force bat
+cargo install --force ripgrep
 cargo install --force skim
 nvim +PackagerInstall +qa
-nvim +'TSInstall rust python' +qa
+nvim +'TSInstall rust python bash c json toml' +qa
 nvim +TSUpdate +qa
